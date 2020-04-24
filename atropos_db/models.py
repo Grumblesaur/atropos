9091a351c2243200d629868eaa22a62508023e14
abpriest <abpriest@gmail.com>
2020-04-24 16:08:45 -0400

    initial setup for db app

---
+++ atropos_db/models.py
@@ -0,0 +1,20 @@
+from django.db.models import Model, CharField, IntegerField, TextField
+
+
+class Variable(Model):
+    owner_id = IntegerField()
+
+    CORE = 'core'
+    GLOBAL = 'global'
+    SERVER = 'server'
+    PRIVATE = 'private'
+    VARIABLE_TYPES = [
+        (CORE, 'core'),
+        (GLOBAL, 'global'),
+        (SERVER, 'server'),
+        (PRIVATE, 'private'),
+    ]
+    var_type = CharField(max_length=7, choices=VARIABLE_TYPES, default=SERVER)
+
+    value_string = TextField()
+    name = CharField(max_length=2000)
--- db_config/settings.py
+++ db_config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'atropos_db.apps.AtroposDbConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EDT'
 
 USE_I18N = True
 
