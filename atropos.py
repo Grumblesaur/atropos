dfa21932fff2c725116c3166427b829fdb0847ed
James Murphy <james.f.murphy.one@gmail.com>
2020-05-09 16:58:20 -0400

    (#27) shorten it again

--- atropos.py
+++ atropos.py
@@ -20,7 +20,7 @@ command_parser = commands.CmdParser()
 @client.event
 async def on_ready():
   activity_type = discord.ActivityType.listening
-  name = "+atropos help quickstart"
+  name = "+help quickstart"
   activity = discord.Activity(type=activity_type, name=name)
   await client.change_presence(activity=activity)
 
