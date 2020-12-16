269f6a7456ba0286d5c837a6db4b0a240919da0b
James Murphy <james.f.murphy.one@gmail.com>
2020-12-16 11:20:53 -0500

    update IntegerValidator to correctly acccept its digit limit argument

--- dicelang/validator.py
+++ dicelang/validator.py
@@ -1,6 +1,6 @@
 class IntegerValidator(object):
-  def __init__(self, max_digit_length=8,exc_type=Exception):
-    self.size_limit = 8
+  def __init__(self, max_digit_length, exc_type=Exception):
+    self.size_limit = max_digit_length
     self.exc_type = exc_type
   
   def validate(self, an_integer, msg='Integer too large!'):
